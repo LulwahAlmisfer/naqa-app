d463871477234195f2ccfc9d7e7a1582d534f39f
Lulwah Almisfer <lulukalmisfer@hotmail.com>
2025-03-01 14:07:27 +0300

    feat(AsyncCompanyLogoView): get logos from api + caching the images

--- Naqa/Views/Components/AsyncCompanyLogoView.swift
+++ Naqa/Views/Components/AsyncCompanyLogoView.swift
@@ -6,36 +6,38 @@
 //
 
 import SwiftUI
-
+import Combine
 
 struct AsyncCompanyLogoView: View {
+    @StateObject private var loader = ImageLoader()
+
     var ticker: String
     var urlString: String {
-      "https://web.alrajhi-capital.sa/stock-images/\(ticker).webp"
+        "https://web.alrajhi-capital.sa/stock-images/\(ticker).webp"
     }
-    
+
     var body: some View {
-        AsyncImage(url: URL(string: urlString), transaction: Transaction(animation: .easeInOut)) { phase in
-            switch phase {
-            case .success(let image):
-                image
+        Group {
+            if let image = loader.image {
+                Image(uiImage: image)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
-            case .empty:
+            } else if loader.hasFailed {
+                placeHolderView
+            } else {
                 ProgressView()
-                
-            case .failure:
-                placeHolderView
-                
-            @unknown default:
-                placeHolderView
             }
         }
         .frame(width: 30, height: 30)
-        .clipShape(.circle)
-        
+        .clipShape(Circle())
+        .onAppear {
+            if let url = URL(string: urlString) {
+                loader.loadImage(from: url)
+            }
+        }
+        .onDisappear { loader.cancel() }
     }
-    
+
     var placeHolderView: some View {
         Group {
             if UIImage(named: ticker) != nil {
@@ -48,7 +50,7 @@ struct AsyncCompanyLogoView: View {
                     Circle()
                         .fill(Color.gray.opacity(0.5))
                         .frame(width: 30, height: 30)
-                    
+
                     Text(ticker)
                         .bold()
                         .foregroundColor(.white)
@@ -57,5 +59,44 @@ struct AsyncCompanyLogoView: View {
             }
         }
     }
-    
+}
+
+class ImageLoader: ObservableObject {
+    @Published var image: UIImage?
+    @Published var hasFailed = false
+    private var url: URL?
+    private var cancellable: AnyCancellable?
+
+    func loadImage(from url: URL) {
+        guard self.url != url, image == nil else { return }
+
+        self.url = url
+        self.hasFailed = false
+
+        if let cachedImage = ImageCache.shared.object(forKey: url as NSURL) {
+            self.image = cachedImage
+            return
+        }
+
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .map { UIImage(data: $0.data) }
+            .replaceError(with: nil)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] image in
+                if let image = image {
+                    ImageCache.shared.setObject(image, forKey: url as NSURL)
+                    self?.image = image
+                } else {
+                    self?.hasFailed = true
+                }
+            }
+    }
+
+    func cancel() {
+        cancellable?.cancel()
+    }
+}
+
+class ImageCache {
+    static let shared = NSCache<NSURL, UIImage>()
 }
