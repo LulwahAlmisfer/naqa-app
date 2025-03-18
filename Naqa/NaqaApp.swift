86e1e0cace0985ed85a88da0ec973d0814c156e0
Lulwah Almisfer <con-lalmasfer@pif.gov.sa>
2025-03-18 15:38:29 +0300

    Initial Commit

---
+++ Naqa/NaqaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaqaApp.swift
+//  Naqa
+//
+//  Created by Lulwah almisfer on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct NaqaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
