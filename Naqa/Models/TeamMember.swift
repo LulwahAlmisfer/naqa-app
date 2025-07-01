039f4d436f37d8f0d7d83fa8a6500fdf5328fd81
Lulwah Almisfer <lulukalmisfer@hotmail.com>
2025-07-01 16:57:33 +0300

    translate the app

--- Naqa/Models/TeamMember.swift
+++ Naqa/Models/TeamMember.swift
@@ -5,18 +5,18 @@
 //  Created by Lulwah almisfer on 01/03/2025.
 //
 
-import Foundation
+import UIKit
 
 struct TeamMember: Identifiable {
     let id = UUID()
-    let name: String
-    let role: String
+    let name: LocalizedStringResource
+    let role: LocalizedStringResource
     let image: String
     let linkedInURL: String
 }
 
 let teamMembers: [TeamMember] = [
     TeamMember(name: "أروى المطرفي", role: "مصممة واجهة المستخدم", image: "person3", linkedInURL: "https://www.linkedin.com/in/arwa-a-b1b85b18b/"),
-    TeamMember(name: "عبدالله القحطاني", role: "مطور الباكيند", image: "person2", linkedInURL: "https://www.linkedin.com/in/anqahtani/"),
-    TeamMember(name: "لولو المسفر", role: "مطورة التطبيق", image: "person1", linkedInURL: "https://www.linkedin.com/in/lulwahalmisfer/"),
+    TeamMember(name: "عبدالله القحطاني", role: "Backend Developer", image: "person2", linkedInURL: "https://www.linkedin.com/in/anqahtani/"),
+    TeamMember(name: "لولوه المسفر", role: "iOS Developer", image: "person1", linkedInURL: "https://www.linkedin.com/in/lulwahalmisfer/"),
 ]
